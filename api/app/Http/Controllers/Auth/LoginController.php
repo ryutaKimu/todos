cf5935e1ae7ddbd36582de65a5781e36bb754279
ryutaKimu <rekkoilove@softbank.ne.jp>
2025-04-13 06:03:35 +0900

    ログイン機能実装

--- api/app/Http/Controllers/Auth/LoginController.php
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
-        if(Auth::attempt($request->only('email', 'password'))){
-            $user = User::find(1);
-            $token = $user->createToken('todoApp')->plainTextToken;
-
-            return response()->json(['token' => $token]);
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => '認証失敗です'], 401);
         }
+        $user = Auth::user();
+        $token = $user->createToken('todoApp')->plainTextToken;
 
-        return response()->json(['message' => '認証失敗です'], 401);
+        return response()->json(['token' => $token]);
     }
 }
