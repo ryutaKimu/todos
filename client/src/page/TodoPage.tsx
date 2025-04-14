de21607ce222c345158c07e8bec7ea3df67155a0
ryutaKimu <rekkoilove@softbank.ne.jp>
2025-04-15 03:39:53 +0900

    fix: adjust style css

--- client/src/page/TodoPage.tsx
+++ client/src/page/TodoPage.tsx
@@ -9,7 +9,7 @@ type Todo = {
 };
 
 export const TodoPage: React.FC = () => {
-	const setAuthenticated = useAuthStore((state) => state.setAuthenticated)
+	const setAuthenticated = useAuthStore((state) => state.setAuthenticated);
 	const navigate = useNavigate();
 	const [todos, setTodos] = useState<Todo[]>([]);
 	const [title, setTitle] = useState<string>("");
@@ -36,14 +36,14 @@ export const TodoPage: React.FC = () => {
 		}
 	};
 
-	const deleteTodo = async (id:number)=>{
-		try{
+	const deleteTodo = async (id: number) => {
+		try {
 			await api.delete(`/todo/${id}`);
-			setTodos(prev => prev.filter(todo => todo.id !== id))
-		}catch(error){
-			console.error(error)
+			setTodos((prev) => prev.filter((todo) => todo.id !== id));
+		} catch (error) {
+			console.error(error);
 		}
-	}
+	};
 
 	const handleLogout = async () => {
 		try {
@@ -82,43 +82,77 @@ export const TodoPage: React.FC = () => {
 	}, []);
 
 	return (
-		<>
-			<h1>Todo一覧</h1>
-			<form onSubmit={insertTodo}>
-				<label>Todo追加</label>
+		<div className="container mx-auto p-4">
+			<h1 className="text-3xl font-bold mb-6">Todo一覧</h1>
+			<form onSubmit={insertTodo} className="mb-6">
+				<label className="block text-lg mb-2">Todo追加</label>
 				<input
 					type="text"
 					value={title}
 					onChange={(e) => setTitle(e.target.value)}
+					className="w-1/2 p-2 border border-gray-300 rounded-md"
 				/>
-				<button type="submit">登録</button>
+				<button
+					type="submit"
+					className=" w-32 mt-1 ml-2 p-2 bg-blue-500 text-white rounded-md"
+				>
+					登録
+				</button>
 			</form>
-			<ul>
+			<ul className="space-y-2">
 				{todos.map((todo) => (
-					<li key={todo.id}>
+					<li
+						key={todo.id}
+						className="flex w-1/2 justify-start items-center p-2 border border-gray-300 rounded-md mb-2"
+					>
 						{editId === todo.id ? (
 							<>
 								<input
 									type="text"
 									value={editTitle}
 									onChange={(e) => setEditTitle(e.target.value)}
+									className="w-1/2 p-1 border border-gray-300 rounded-md"
 								/>
-								<button onClick={() => updateTodo(todo.id)}>保存</button>
-								<button onClick={() => setEditId(null)}>キャンセル</button>
+								<button
+									onClick={() => updateTodo(todo.id)}
+									className="w-16 ml-2 p-2  bg-green-500 text-white rounded-md"
+								>
+									保存
+								</button>
+								<button
+									onClick={() => setEditId(null)}
+									className="ml-2 p-2 bg-gray-300 text-white rounded-md"
+								>
+									キャンセル
+								</button>
 							</>
 						) : (
 							<>
-								{todo.title}
-								<button onClick={() => handleEdit(todo)}>編集</button>
+								<span className="w-full">{todo.title}</span>
+								<button
+									onClick={() => handleEdit(todo)}
+									className="w-16 ml-4 p-2 bg-yellow-500 text-white rounded-md"
+								>
+									編集
+								</button>
 							</>
 						)}
-						<button onClick={()=>deleteTodo(todo.id)}>削除</button>
+						<button
+							onClick={() => deleteTodo(todo.id)}
+							className=" w-16 ml-2 p-2 bg-red-500 text-white rounded-md"
+						>
+							削除
+						</button>
 					</li>
 				))}
 			</ul>
-			<button type="submit" onClick={handleLogout}>
+			<button
+				type="button"
+				onClick={handleLogout}
+				className="mt-6 p-2 bg-red-600 text-white rounded-md"
+			>
 				ログアウト
 			</button>
-		</>
+		</div>
 	);
 };
